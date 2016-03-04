51fc94ea5b1660d18b0d4e1e5415247723e729ef
Chris Smothers <chris.smothers@gmail.com>
2016-03-04 14:39:58 -0500

    basic product hunt scraper

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,12 +5,14 @@ class Article
   include Mongoid::Document
   include Mongoid::Timestamps
   # include Mongoid::Attributes::Dynamic
-  field :source, type: String # e.g. the original source of the article like New York Times or Medium
-  index  source: 1
+  field :categories, type: Array, default: [] # e.g. tech, podcasts, books
+  index  categories: 1
   field :url, type: String # e.g. the most direct link to the article, try to click through short/tracking links and scrape the original if possible
   index  url: 1
   field :title, type: String
   index  title: 1
+  field :source, type: String # e.g. the original source of the article like New York Times or Medium
+  index  source: 1
   field :description, type: String
   index  description: 1
   field :image_url, type: String
@@ -27,8 +29,6 @@ class Article
   index  comments: 1
   field :normalized_popularity, type: Integer # calculated by scraper, ideally between 0 - 100, based on upvotes, comments, etc.
   index  normalized_popularity: 1
-  field :categories, type: Array, default: [] # e.g. tech, podcasts, books
-  index  categories: 1
 
   # field :scrape_meta, type: Hash # information that might be used to recreate the scrape that got this data
   field :scrape_times, type: Array, default: []
@@ -41,14 +41,47 @@ class Article
   accepts_nested_attributes_for :sources
 
   def self.create_or_update attrs
+    # Normalize the url for consistency
+    attrs[:url] = Article.normalize_url(attrs[:url])
+    # Try to find a matching article
     article = Article.find_by(url: attrs[:url]) rescue nil
+    # Update or create
     if article
       raise "Failed to merge Article attributes" unless article.merge_attrs(attrs)
       article.save!
     else
       article = Article.create!(attrs)
     end
+    # Append scrape time
     article.push(scrape_times: DateTime.now)
+  end
+
+  def self.normalize_url url
+    # Follow redirects to get actual url
+    url = Article.resolve_redirects(url)
+    # Normalize url by removing query and fragment
+    parsed = URI::parse(url)
+    parsed.fragment = parsed.query = nil
+    return parsed.to_s
+  end
+
+  def self.resolve_redirects(url)
+    response = Article.fetch_response(url, method: :head)
+    if response
+        return response.to_hash[:url].to_s
+    else
+        return nil
+    end
+  end
+
+  def self.fetch_response(url, method: :get)
+    conn = Faraday.new do |b|
+      b.use FaradayMiddleware::FollowRedirects;
+      b.adapter :net_http
+    end
+    return conn.send method, url
+    rescue Faraday::Error, Faraday::Error::ConnectionFailed => e
+    return nil
   end
 
   def merge_attrs attrs
@@ -58,17 +91,23 @@ class Article
     setable_attrs = attrs.slice(*setable_fields)
     self.attributes = setable_attrs
     # Merge categories
-    self.categories = (self.categories || []) | attrs[:categories] if attrs.has_key?(:categories)
+    self.categories = (self.categories || []) | attrs[:categories].map(&:downcase) if attrs.has_key?(:categories)
     # Merge sources
     if attrs.has_key?(:sources)
       setable_fields -= [:source]
       setable_fields += [:name, :url]
       attrs[:sources].each do |s|
+        # Normalize the url
+        s[:url] = Article.normalize_url(s[:url]) if s.has_key?(:url)
+        # Find the source by name or create a new one
         source = self.sources.find_by(name: s[:name]) rescue self.sources.new(name: s[:name])
+        # Set the setable attributes
         setable_attrs = s.slice(*setable_fields)
         source.attributes = setable_attrs
-        source.categories = (source.categories || []) | s[:categories] if s.has_key?(:categories)
-        # Set a temporary title and published_at if none exist
+        # Merge categories
+        source.categories = (source.categories || []) | s[:categories].map(&:downcase) if s.has_key?(:categories)
+        self.categories = (self.categories || []) | s[:categories].map(&:downcase) if s.has_key?(:categories)
+        # Set a temporary title and published_at on the parent Article if none exist
         self.title = s[:title] unless self.title
         self.published_at = s[:published_at] unless self.published_at
         # Append the scrape time
--- app/workers/ProductHuntScraper.rb
+++ app/workers/ProductHuntScraper.rb
@@ -1,36 +1,36 @@
-# class ProductHuntScraper
-#   include Sidekiq::Worker
-#   include Sidetiq::Schedulable
-#
-#   sidekiq_options retry: false
-#
-#   recurrence { hourly.minute_of_hour(0, 30) }
-#
-#   def perform
-#     urls = [
-#       'https://www.producthunt.com/feed.atom'
-#     ]
-#     urls.each { |url| handle_url(url) }
-#   end
-#
-#   def handle_url url
-#     # xml = Faraday.get(url).body
-#     # feed = Feedjira::Feed.parse xml
-#     # feed.entries.each do |e|
-#     #   title_attrs = e.title.split("&#8211;")
-#     #
-#     #   attrs = {}
-#     #   attrs[:source] = "Hacker News"
-#     #   attrs[:points] = title_attrs.shift().to_i
-#     #   attrs[:title] = title_attrs.join("-").strip
-#     #   attrs[:url] = e.links.first
-#     #   attrs[:comments_url] = e.entry_id
-#     #   attrs[:author] = e.author
-#     #   attrs[:published_at] = DateTime.parse(e.published.to_s)
-#     #   attrs[:comments] = e.content.scan(/">.{0,1}(\d+).{0,1}comments/).flatten.first.to_i
-#     #   attrs[:normalized_popularity] = (attrs[:points] + attrs[:comments])/15 # assumes 1500 as soft maximum points + comments
-#     #
-#     #   Article.delay(queue: 'default', retry: false).create_or_update(attrs)
-#     # end
-#   end
-# end
+class ProductHuntScraper
+  include Sidekiq::Worker
+  include Sidetiq::Schedulable
+
+  sidekiq_options retry: false
+
+  recurrence { hourly.minute_of_hour(0, 30) }
+
+  def perform
+    urls = [
+      'https://www.producthunt.com/feed.atom'
+    ]
+    urls.each { |url| handle_url(url) }
+  end
+
+  def handle_url url
+    xml = Faraday.get(url).body
+    feed = Feedjira::Feed.parse xml
+    feed.entries.each do |e|
+      title_attrs = e.title.split("&#8211;")
+
+      attrs = { sources: [{}] }
+      attrs[:sources][0][:name] = "Product Hunt"
+      attrs[:sources][0][:title] = e.title
+      attrs[:sources][0][:categories] = e.categories
+      attrs[:sources][0][:description] = e.content
+      attrs[:url] = e.url
+      attrs[:sources][0][:url] = e.content.scan(/"(.*)">Discussion/).flatten.first
+      attrs[:sources][0][:author] = e.author
+      attrs[:sources][0][:published_at] = DateTime.parse(e.published.to_s)
+      # TODO: points, comments, normalized_popularity
+
+      Article.delay(queue: 'default', retry: false).create_or_update(attrs)
+    end
+  end
+end
