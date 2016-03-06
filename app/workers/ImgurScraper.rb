cbd90c8598cd6f28a343cfdb03723eeefd89db93
Chris Smothers <chris.smothers@gmail.com>
2016-03-06 01:17:11 -0500

    imgur

---
+++ app/workers/ImgurScraper.rb
@@ -0,0 +1,33 @@
+class ImgurScraper
+  include Sidekiq::Worker
+  include Sidetiq::Schedulable
+
+  sidekiq_options retry: false
+
+  recurrence { hourly.minute_of_hour(0, 30) }
+
+  def perform
+    urls = [
+      'http://feeds.feedburner.com/ImgurGallery?format=xml'
+    ]
+    urls.each { |url| handle_url(url) }
+  end
+
+  def handle_url url
+    feed = Article.parse_feed url
+    feed.entries.each_with_index do |e, i|
+      attrs = { sources: [{}] }
+      attrs[:source] = "Imgur"
+      attrs[:title] = e.title
+      attrs[:url] = e.url
+      # attrs[:author] = e.author
+      # attrs[:published_at] = DateTime.parse(e.published.to_s)
+      attrs[:image_url] = e.image
+      attrs[:summary] = e.summary
+      attrs[:description] = Article.html_to_s(e.summary)
+      # attrs[:categories] = e.categories
+
+      Article.delay_for((i*2).seconds, queue: 'default', retry: 2).create_or_update(attrs)
+    end
+  end
+end
